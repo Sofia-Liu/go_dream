027f7cee00f59747ab922054669fedd56e0de850
momo <momo@192.168.1.7>
2022-12-15 23:10:03 +0800

    the first commit

---
+++ chapter_1/main.go
@@ -0,0 +1,46 @@
+// 汉字unicode编码中1个汉字2个字节，utf-8中1个汉字3个字节
+package main
+
+//import (
+//	"fmt"
+//	"strings"
+//)
+func main() {
+	//var str = "hello 你好啊"
+	//var str1 string = "hello go"
+	//var a float64 = 2.12
+	//var b int = 4
+	//Time是类型，Now是值，Year是方法
+	//var now time.Time = time.Now()
+	//var year int = time.Now().Year()
+	//fmt.Println("len(str):", len(str))
+	//fmt.Println("RuneCountInString", utf8.RuneCountInString(str))
+	//fmt.Println("rune", len([]rune(str)))
+	//fmt.Println(strings.Title(str1))
+	//fmt.Println(math.Floor(a))
+	//fmt.Println(reflect.TypeOf(a))
+	//if a < float64(b) {
+	//	fmt.Println(reflect.TypeOf(b))
+	//}
+	//calcu()
+	//fmt.Println(reflect.TypeOf(time.Now()))
+	//fmt.Println(year)
+	//broken := "G# r#cks!"
+	//replacer := strings.NewReplacer("#", "o")
+	//fixed := replacer.Replace(broken)
+	//fixed := strings.NewReplacer().Replace(broken)
+	//fmt.Println(fixed)
+}
+
+//func calcu() {
+//	var price int = 100
+//	fmt.Println("Price is", price, "dollars.")
+//	var taxRate float64 = 0.08
+//	var tax float64 = float64(price) * taxRate
+//	fmt.Println("Tax is", tax, "dollars.")
+//	var total float64 = float64(price) + tax
+//	fmt.Println("Total cost is", total, "dollars.")
+//	var availableFunds int = 120
+//	fmt.Println(availableFunds, "dollars available.")
+//	fmt.Println("Within budget?", total <= float64(availableFunds))
+//}
